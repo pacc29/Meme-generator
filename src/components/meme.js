cae5fd06959cd6af591375c82ccdefb255bdc863
Vaunt <vauntpy@gmail.com>
2022-07-15 14:32:18 -0500

    update

--- src/components/meme.js
+++ src/components/meme.js
@@ -2,27 +2,34 @@ import React from "react";
 import memesdata from "../memesdata";
 
 export default function Meme() {
-  let [meme, setMeme] = React.useState("");
+  let [meme, setMeme] = React.useState("https://i.imgflip.com/3si4.jpg");
+  let [upperText, setUpperText] = React.useState("")
+  let [lowerText, setLowerText] = React.useState("")
 
   let getImg = () => {
     let memesArray = memesdata.data.memes;
+
     let randNumber = Math.floor(Math.random() * memesArray.length);
-    setMeme(memesArray[randNumber].url);
+    setMeme(memesArray[randNumber].url); 
   };
 
+  let display = () => {
+    setUpperText(document.querySelector(".upperInput").value);
+    setLowerText(document.querySelector(".lowerInput").value);
+    }
   return (
     <section className="main-meme">
       <div className="inputs">
-        <input className="input" type="text" placeholder="Top text"></input>
-        <input className="input" type="text" placeholder="Bottom text"></input>
+        <input className="input upperInput" onInput={display} type="text" placeholder="Top text"></input>
+        <input className="input lowerInput" onInput={display} type="text" placeholder="Bottom text"></input>
       </div>
       <button className="button" onClick={getImg}>
         Get a new meme image
       </button>
       <div className="meme">
         <img className="img-meme" alt="meme" src={meme} />
-        {/* <span className="meme-text meme-text--upper">shut up</span>
-        <span className="meme-text meme-text--lower">and take my money</span> */}
+        <span className="meme-text meme-text--upper">{upperText}</span>
+	<span className="meme-text meme-text--lower">{lowerText}</span> 
       </div>
     </section>
   );
